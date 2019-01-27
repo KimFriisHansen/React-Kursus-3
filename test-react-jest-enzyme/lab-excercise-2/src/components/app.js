d26bbf8255221bd01af5898c3a8ef664a57b5b0d
Kim Friis Hansen <kim@kimfriishansen.dk>
2019-01-27 23:08:11 +0100

    Add files via upload

---
+++ test-react-jest-enzyme/lab-excercise-2/src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CommentBox from 'components/comment-box';
+import CommentList from 'components/comment-list';
+
+export default () => {
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
+};
